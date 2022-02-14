9d7652aa6871e7b9d73c464b6761706e0b317805
Alonso Isidoro Román <alonsoir@gmail.com>
2022-02-14 13:02:20 +0100

    added kafka ksql course content

---
+++ kafka course Stephane/code kafka-connect-Stephane/ip.php
@@ -0,0 +1,7 @@
+<meta property=»og:description» content=»<?php
+
+echo "HTTP_CLIENT_IP: ". $_SERVER['HTTP_CLIENT_IP'];
+echo "HTTP_X_FORWARDED_FOR: ". $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+echo "REMOTE_ADDR: ". $_SERVER['REMOTE_ADDR'];
+?>» />
